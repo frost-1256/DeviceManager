729f10aae75b3543b65eddcc742428d9570a3121
frost-1256 <habanero.iori@gmail.com>
2025-03-13 22:06:31 +0900

    Initial commit

---
+++ app/src/main/java/si/f5/harukaze/devicemanager/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package si.f5.harukaze.devicemanager.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
